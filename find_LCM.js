8081ad9dc08bd372be8999a77d1fa7e10a527c0e
Apoorv <apoorvsharma494@gmail.com>
2022-03-20 13:36:29 +0530

    commit

--- find_LCM.js
+++ find_LCM.js
@@ -1,1 +1,25 @@
-console.log
+const num1 = prompt('Enter a positive number: ');
+const num2 = prompt('Enter a positive number: ');
+
+// for (i = 1; i <= 10; i++) {
+//     var fact = num1 * i
+//     console.log(fact)
+// }
+
+// for (i = 1; i <= 10; i++) {
+//     var fact2 = num2 * i
+//     console.log(fact2)
+// }
+
+// if (fact === fact2) {
+//     console.log(`LCM is ${fact}`)
+// }
+
+let num3 = num1 * num2;
+
+for (var i = 1; i <= num3; i++) {
+    if (i % num1 == 0 && i % num2 == 0) {
+        break
+    }
+}
+console.log(i)
