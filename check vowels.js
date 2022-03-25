9ff28ffaa1a8e225073f3e0345e2b270688cce0a
Apoorv <apoorvsharma494@gmail.com>
2022-03-25 23:52:43 +0530

    commit

--- check vowels.js
+++ check vowels.js
@@ -10,8 +10,37 @@ function countVowel(str) {
 }
 
 // take input
-const string = prompt('Enter a string: ');
+const string = "dog";
 
 const result = countVowel(string);
 
 console.log(result);
+
+// ---------------------check vowels using loop---------------------------
+
+// program to count the number of vowels in a string
+
+// defining vowels
+
+function countVowel(str) {
+    // initialize count
+    const vowels = ["a", "e", "i", "o", "u"]
+    let count = 0;
+
+    // loop through string to test if each character is a vowel
+    for (let letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            count++;
+        }
+    }
+
+    // return number of vowels
+    return count
+}
+
+// take input
+const string2 = prompt('Enter a string: ');
+
+const result2 = countVowel(string2);
+
+console.log(result2);
