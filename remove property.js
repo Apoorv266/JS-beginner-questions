bf2ff21f09a8e69a1b692596440cf22d88ca6c78
Apoorv <apoorvsharma494@gmail.com>
2022-03-26 22:43:19 +0530

    commit

---
+++ remove property.js
@@ -0,0 +1,19 @@
+const student = { 
+    name: 'John',
+    age: 20,
+    hobbies: ['reading', 'games', 'coding'],
+    greet: function() {
+        console.log('Hello everyone.');
+    },
+    score: {
+        maths: 90,
+        science: 80
+    }
+};
+
+delete student.age;
+// or
+delete student['name'];
+
+console.log(student);
+
