df164dee131457b608580f2309fe873d9ceccd9f
Apoorv <apoorvsharma494@gmail.com>
2022-03-27 23:37:10 +0530

    commit

---
+++ clone object.js
@@ -0,0 +1,11 @@
+// write a program that clones an object.
+
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+const copy = person;
+console.log(copy); // {name: "John", age: 21}
+
+// the copy variable has the same value as the person object.
