b2f8136dc188b93d53f9d4e3dd390d52c68f5e79
Apoorv <apoorvsharma494@gmail.com>
2022-03-27 23:31:56 +0530

    commit

--- object key exist.js
+++ object key exist.js
@@ -1,5 +1,8 @@
 // program to check if a key exists
 
+
+// ----------------------METHOD 1------------------------------------
+
 let obj = {
     name : "Apoorv",
     age : "21",
@@ -17,3 +20,23 @@ else{
 }
 
 // in operator is used to check if a key exists in an object. The in operator returns true if the specified key is in the object, otherwise it returns false.
+
+
+// ----------------------METHOD 2------------------------------------
+
+const person = {
+    id: 1,
+    name: 'John',
+    age: 23
+}
+
+let keyHas2 = person.hasOwnProperty('name');
+
+if(keyHas2) {
+    console.log('The key exists.');
+}
+else {
+    console.log('The key does not exist.');
+}
+
+// hasOwnProperty() method is used to check if a key exists in an object. The hasOwnProperty() method returns true if the specified key is in the object, otherwise it returns false.
