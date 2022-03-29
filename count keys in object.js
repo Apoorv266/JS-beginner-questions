a5d469f1e4040dfa597a2515271afcf24e5a706e
Apoorv <apoorvsharma494@gmail.com>
2022-03-29 23:44:08 +0530

    commit

--- count keys in object.js
+++ count keys in object.js
@@ -15,3 +15,22 @@ for (let key in student) {
 }
 
 console.log(count);
+
+
+//------Number of Key in an Object Using Object.key()--------------
+
+const student2 = { 
+    name: 'John',
+    age: 20,
+    hobbies: ['reading', 'games', 'coding'],
+};
+
+
+// count the key/value
+const result = Object.keys(student).length;
+
+
+console.log(result);
+
+//The Object.keys() method returns an array of a given object's own enumerable property names i.e. ["name", "age", "hobbies"].
+
