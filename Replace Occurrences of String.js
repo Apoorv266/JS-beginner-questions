d41267d70d987f2360ce5ba910fbaa786383460a
Apoorv <apoorvsharma494@gmail.com>
2022-03-29 23:57:32 +0530

    commit

--- Replace Occurrences of String.js
+++ Replace Occurrences of String.js
@@ -10,3 +10,13 @@ const newText = string.replace(regex, 'blue');
 
 // display the result
 console.log(newText);  //Mr blue has a blue house and a blue car
+
+//---------Replace All Occurrence of String Using built-in Method----------------
+
+// program to replace all occurrence of a string
+
+const string2 = 'Mr red has a red house and a red car';
+
+const result = string2.split('red').join('blue');
+
+console.log(result);
