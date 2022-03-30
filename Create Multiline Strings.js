fe1bad344705a38be6b0b3d1effd37a2bc17b993
Apoorv <apoorvsharma494@gmail.com>
2022-03-30 23:33:09 +0530

    commit

--- Create Multiline Strings.js
+++ Create Multiline Strings.js
@@ -1,5 +1,7 @@
 // write a JavaScript program that will create multiline strings
 
+//-------------Create Multiline Strings Using +----------------------------------------
+
 // using the + operator
 const message = 'This is a long message\n' +
     'that spans across multiple lines\n' +
@@ -8,3 +10,21 @@ const message = 'This is a long message\n' +
 console.log(message);
 
 // The escape character \n is used to break the line.
+
+
+//-------------Create Multiline Strings Using \-----------------------------------------
+
+// using the + operator
+const message2 = 'This is a long message\n \
+that spans across multiple lines\n \
+in the code.'
+
+console.log(message2);
+
+//------------Create Multiline Strings Using Template Literal---------------------------
+
+const message3 = `This is a long message
+that spans across multiple lines
+in the code.`
+
+console.log(message3);
