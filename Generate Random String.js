19a1ffccd5af46956121f72cc707aff6871c2276
Apoorv <apoorvsharma494@gmail.com>
2022-03-31 23:05:50 +0530

    commit

--- Generate Random String.js
+++ Generate Random String.js
@@ -7,6 +7,16 @@ function generateString(length) {
     for ( let i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
+
+   // (Math.floor(Math.random() * charactersLength) generates a random number and give it to characters.charAt which inturn find string character corresponding to that number in "characters" variable
+
     return result;
 }
-console.log(generateString(20));
+
+console.log(generateString(20)); //invoking function
+
+
+//---------------Generate Random Strings Using Built-in Methods--------------------
+
+const result = Math.random().toString(36).substring(2,7);
+console.log(result);
