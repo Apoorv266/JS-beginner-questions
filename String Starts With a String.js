4d13f9a4c3bdb963b26cb13cdea32ce8944467fc
Apoorv <apoorvsharma494@gmail.com>
2022-03-31 23:33:12 +0530

    commit

--- String Starts With a String.js
+++ String Starts With a String.js
@@ -10,3 +10,21 @@ else {
 }
 
 // startsWith() method checks if the string starts with the particular string.
+
+
+
+//-----------------Using lastIndexOf()-------------------------
+
+// program to check if a string starts with another string
+
+const string = 'hello world';
+
+const toCheckString = 'he';
+
+let result = string.lastIndexOf(toCheckString, 0) === 0;
+if(result) {
+    console.warn('The string starts with "he".');
+}
+else {
+    console.warn(`The string does not starts with "he".`);
+}
