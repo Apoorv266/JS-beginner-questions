ae189e6fa0f97619f1020d096921840351a0752d
Apoorv <apoorvsharma494@gmail.com>
2022-04-01 23:31:48 +0530

    commit

---
+++ object to string.js
@@ -0,0 +1,10 @@
+//-----------------Object to String Using JSON.stringify()---------------
+
+const person = {
+    name: 'Jack',
+    age: 27
+}
+
+const result = JSON.stringify(person);
+
+console.log(result);
