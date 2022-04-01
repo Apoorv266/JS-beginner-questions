ab3e48434d9accc2aef91cd3bf1c79be615f8b4b
Apoorv <apoorvsharma494@gmail.com>
2022-04-01 23:55:25 +0530

    commit

---
+++ if String Contains substring.js
@@ -0,0 +1,14 @@
+//-----------------Check String with includes()---------------------
+
+// take input
+const str = prompt('Enter a string:');
+const checkString = prompt('Enter a string that you want to check:');
+
+// check if string contains a substring
+if(str.includes(checkString)) {
+    console.log(`The string contains ${checkString}`);
+} else {
+    console.log(`The string does not contain ${checkString}`);
+}
+
+// The includes() method is case-sensitive. Hence, fun and Fun are different.
