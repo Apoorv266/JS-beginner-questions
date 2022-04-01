b08f6eeeee31e50a5dcb36b7199cb7d61db70852
Apoorv <apoorvsharma494@gmail.com>
2022-04-01 23:41:08 +0530

    commit

--- object to string.js
+++ object to string.js
@@ -8,3 +8,22 @@ const person = {
 const result = JSON.stringify(person);
 
 console.log(result);
+
+//-------------Convert Object to String Using String()--------------------
+
+// program to convert an object to a string
+
+const person2 = {
+    name: 'Jack',
+    age: 27
+}
+
+const result1 = String(person2);
+const result2 = String(person2['name']);
+
+console.log(result1);
+console.log(result2);
+
+console.log(typeof result1);
+
+// When using the String() function on an Object, the converted result will give [object Object].
