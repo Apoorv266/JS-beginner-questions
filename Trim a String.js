4fc749059c9fa520f76a0fe1b258de14588e5881
Apoorv <apoorvsharma494@gmail.com>
2022-04-01 23:22:43 +0530

    commit

--- Trim a String.js
+++ Trim a String.js
@@ -1,8 +1,20 @@
-//-----------------------Trim a String-------------------------------
+//---------------------------Trim a String------------------------------------
 
 
 const string = '      Hello World       ';
 const result = string.trim();
 console.log(result);
 
-// The trim() method removes white space from both sides of the string.
+// The trim() method removes white space from both sides of the string
+
+
+//-------------------Trim a String Using RegEx----------------------------------
+
+
+function trimString(x) {
+    let trimValue = x.replace(/^\s+|\s+$/g,'');
+    return trimValue;
+}
+
+const result2 = trimString('    Hello world    ');
+console.log(result2);
