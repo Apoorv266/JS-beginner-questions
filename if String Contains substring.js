e87bd1eaf34681d1c4fc8479e44979c27d1721c4
Apoorv <apoorvsharma494@gmail.com>
2022-04-03 23:31:14 +0530

    commit

--- if String Contains substring.js
+++ if String Contains substring.js
@@ -13,3 +13,17 @@ if(str.includes(checkString)) {
 
 // The includes() method is case-sensitive
 // if...else statement to check whether a string contains the characters of a specified string.
+
+//---------------Check String with indexOf()--------------------------
+
+// take input
+const str2 = prompt('Enter a string:');
+const checkString2 = prompt('Enter a string that you want to check:');
+
+if(str.indexOf(checkString) !== -1) {
+    console.log(`The string contains ${checkString}`);
+} else {
+    console.log(`The string does not contain ${checkString}`);
+}
+
+// The indexOf() method searches a string and returns the position of the first occurrence. When a substring cannot be found, it returns -1.
