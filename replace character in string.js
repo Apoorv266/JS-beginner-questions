ba694309f28c4983bea057c75e4c7cb10ae3c0af
Apoorv <apoorvsharma494@gmail.com>
2022-04-04 23:01:16 +0530

    commit

---
+++ replace character in string.js
@@ -0,0 +1,7 @@
+//---------------Replace All Instances Of a Character Using Regex------------------
+
+const string = 'Learning JavaScript Program';
+
+const result = string.replace(/a/g, "A");   // first is which one is to be replaced and second is by which it has to be replaced
+
+console.log(result);
