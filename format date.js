42a4bb9d5d4bf8c4cadf6d4e095dda56d0286808
Apoorv <apoorvsharma494@gmail.com>
2022-04-06 23:28:38 +0530

    commit

---
+++ format date.js
@@ -0,0 +1,39 @@
+// get current  date
+let currentDate = new Date();
+console.log(currentDate)
+
+// get the day from the date
+let day = currentDate.getDate();
+console.log("day is",day)
+
+// get the month from the date
+// + 1 because month starts from 0
+let month = currentDate.getMonth() + 1;
+console.log("month is",month)
+
+// get the year from the date
+let year = currentDate.getFullYear();
+console.log("year is",year)
+
+// if day is less than 10, add 0 to make consistent format
+if (day < 10) {
+    day = '0' + day;
+}
+
+// if month is less than 10, add 0
+if (month < 10) {
+    month = '0' + month;
+}
+
+// display in various formats
+const formattedDate1 = month + '/' + day + '/' + year;
+console.log(formattedDate1);
+
+const formattedDate2 = month + '-' + day + '-' + year;
+console.log(formattedDate2);
+
+const formattedDate3 = day + '-' + month + '-' + year;
+console.log(formattedDate3);
+
+const formattedDate4 = day + '/' + month + '/' + year;
+console.log(formattedDate4);
