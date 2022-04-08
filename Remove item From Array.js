99a5d077acabc4848958e67489ee1ab53e590c6e
Apoorv <apoorvsharma494@gmail.com>
2022-04-08 23:08:26 +0530

    commit

--- Remove item From Array.js
+++ Remove item From Array.js
@@ -5,14 +5,29 @@ function removeItemFromArray(array, n) {
     const index = array.indexOf(n);
 
     // if the element is in the array, remove it
-    if(index > -1) {
+    if (index > -1) {
 
         // remove item
-        array.splice(index, 1);
+        array.splice(index, 1); //splice() method is used to remove the element from an array.
     }
     return array;
 }
 
-const result = removeItemFromArray([1, 2, 3 , 4, 5], 2);
+const result = removeItemFromArray([1, 2, 3, 4, 5], 2);
 
 console.log(result);
+
+//..............................Using For Loop...............................
+
+function removeItemFromArray(array, n) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== n) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+
+const result2 = removeItemFromArray([1, 2, 3 , 4 , 5], 2);
+console.log(result2);
