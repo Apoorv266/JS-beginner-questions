b1dcc168af8c864c66c5e67f7b247b2cebb454e6
Apoorv <apoorvsharma494@gmail.com>
2022-04-08 23:17:32 +0530

    commit

--- Remove item From Array.js
+++ Remove item From Array.js
@@ -22,12 +22,12 @@ console.log(result);
 function removeItemFromArray(array, n) {
     const newArray = [];
     for (let i = 0; i < array.length; i++) {
-        if (array[i] !== n) {
+        if (array[i] !== n) { //While iterating through the elements of the array, if the item to remove does not match with the array element, that element is pushed to newArray. (if the array is not equal to array value then it is pushed to newArray)
             newArray.push(array[i]);
         }
     }
     return newArray;
 }
 
-const result2 = removeItemFromArray([1, 2, 3 , 4 , 5], 2);
+const result2 = removeItemFromArray([1, 2, 3, 4, 5], 2);
 console.log(result2);
