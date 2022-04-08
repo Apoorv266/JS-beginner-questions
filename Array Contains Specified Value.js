9f0a91c84a17863fcc2e71f28207dd8db5430983
Apoorv <apoorvsharma494@gmail.com>
2022-04-08 23:56:52 +0530

    commit

--- Array Contains Specified Value.js
+++ Array Contains Specified Value.js
@@ -10,3 +10,16 @@ if (hasValue) {
 } else {
     console.log('Array does not contain a value.');
 }
+
+
+//...................Check Array Using indexOf()............................
+
+const array2 = ['you', 'will', 'learn', 'javascript'];
+const hasValue2 = array.indexOf('javascript') !== -1; //The indexOf() method searches an array and returns the position of the first occurrence. If the value cannot be found, it returns -1.
+
+// check the condition
+if(hasValue2) {
+    console.log('Array contains a value.');
+} else {
+    console.log('Array does not contain a value.');
+}
