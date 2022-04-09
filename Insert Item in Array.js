0b8b267d379a38d94aa86296fddad51e6fc27f30
Apoorv <apoorvsharma494@gmail.com>
2022-04-09 23:55:56 +0530

    commit

--- Insert Item in Array.js
+++ Insert Item in Array.js
@@ -22,3 +22,31 @@ function insertElement() {
 
 //-----------------------Add Item to Array Using for Loop---------------------------------
 
+// program to insert an item at a specific index into an array
+
+function insertElement() {
+    let array = [1, 2, 3, 4];
+
+    // index to add to
+    let index = 3;
+
+    // element that you want to add
+    let element = 8;
+  
+    for (let i = array.length; i > index; i--) {
+
+        //shift the elements that are greater than index
+        array[i] = array[i-1];
+    }
+
+    // insert element at given index
+    array[index] = element;
+
+    console.log(array);
+}
+
+insertElement();
+
+// The for loop is used to iterate through the array elements.
+// The element is added to the given index.
+// All the elements whose index is greater than the given index are shifted one step to the right.
