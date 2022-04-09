54631cf48670a9d7274a5524a76b7b3b7fd582fa
Apoorv <apoorvsharma494@gmail.com>
2022-04-09 23:49:26 +0530

    commit

--- Insert Item in Array.js
+++ Insert Item in Array.js
@@ -13,3 +13,9 @@ function insertElement() {
     console.log(array);
 
 }
+
+// In the splice() method,
+
+// The first argument specifies the index where you want to insert an item.
+// The second argument (here 0) specifies the number of items to remove.
+// The third argument specifies the element that you want to add to an array.
