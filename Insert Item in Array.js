c17b724a1104bcfb2bd7e33f130561cb7c14a6aa
Apoorv <apoorvsharma494@gmail.com>
2022-04-09 23:48:22 +0530

    commit

--- Insert Item in Array.js
+++ Insert Item in Array.js
@@ -1,1 +1,15 @@
 //--------------Add Item to Array Using splice()-----------------------------
+
+function insertElement() {
+    let array = [1, 2, 3, 4, 5];
+
+    // index to add to
+    let index = 3;
+
+    // element that you want to add
+    let element = 8;
+
+    array.splice(index, 0, element);
+    console.log(array);
+
+}
