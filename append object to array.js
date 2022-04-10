0610bab97d3c3c30c20e35b654422acc0d3f63ef
Apoorv <apoorvsharma494@gmail.com>
2022-04-10 23:11:22 +0530

    commit

--- append object to array.js
+++ append object to array.js
@@ -21,3 +21,33 @@ insertObject(array, object);
 
 //The push() method adds an item to the end of an array.
 
+
+//......................Append Object to Array Using splice()...............
+
+// program to append an object to an array
+
+function insertObject(arr, obj) {
+
+    // find the last index
+     let index = arr.length;
+ 
+     // appending object to end of array
+     arr.splice(index, 0, object);
+     
+     console.log(arr);
+ }
+ 
+ // original array
+ let array2 = [1, 2, 3];
+ 
+ // object to add
+ let object2 = {x: 12, y: 8};
+ 
+ // call the function
+ insertObject(array2, object2);
+
+//  In the splice() method,
+
+// The first argument represents the index where you want to insert an item.
+// The second argument represents the number of items to be removed (here, 0).
+// The third argument represents the element that you want to add to an array.
