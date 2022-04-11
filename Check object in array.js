d15bf650faadf9865c297068e2d32daf91554603
Apoorv <apoorvsharma494@gmail.com>
2022-04-11 23:34:48 +0530

    commit

---
+++ Check object in array.js
@@ -0,0 +1,21 @@
+//....................Check Array Using Array.isArray()............................
+function checkObject(arr) {
+
+    // check if arr is array
+    const result = Array.isArray(arr);
+
+    if(result) {
+        console.log(`[${arr}] is an array.`);
+    }
+    else {
+        console.log(`${arr} is not an array.`);
+    }
+
+}
+const array = [1, 2, 3];
+
+// call the function
+checkObject(array);
+
+// Array.isArray() method is used to check if an object is an array
+// The Array.isArray() method returns true
