ff59994407803e991bb4bdc93600f3222a9b6212
Apoorv <apoorvsharma494@gmail.com>
2022-04-12 23:22:36 +0530

    commit

--- Add element to start array.js
+++ Add element to start array.js
@@ -13,3 +13,27 @@ const array = [1, 2, 3];
 
 // passing array argument
 addElement(array);
+
+//.................Add Element to Array Using splice()........................
+
+// program to add element to an array
+
+function addElement(arr) {
+
+    // adding element to array
+    arr.splice(0, 0, 4);
+    
+    console.log(arr);
+}
+const array2 = [1, 2, 3];
+
+// calling the function
+addElement(array2);
+
+// In the splice() method,
+// The first argument is the index of an array where you want to add an element.
+// The second argument is the number of elements that you want to remove from the index element.
+// The third argument is the element that you want to add to the array.
+
+
+//.............Add Element to Array Using Spread Operator................
