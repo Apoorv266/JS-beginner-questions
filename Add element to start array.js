b8861add2ec23190545445d2564ff611a38f31d0
Apoorv <apoorvsharma494@gmail.com>
2022-04-12 23:16:32 +0530

    commit

---
+++ Add element to start array.js
@@ -0,0 +1,15 @@
+//..................Add Element to Array Using unshift().......................
+
+function addElement(arr) {
+
+    // adding new array element
+    arr.unshift(4);
+    
+    console.log(arr);
+}
+
+const array = [1, 2, 3];
+// calling the function
+
+// passing array argument
+addElement(array);
