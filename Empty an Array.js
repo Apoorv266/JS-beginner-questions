11b08059039ca31e2de46603a9f63e6dec7e5768
Apoorv <apoorvsharma494@gmail.com>
2022-04-12 23:09:37 +0530

    commit

--- Empty an Array.js
+++ Empty an Array.js
@@ -1,3 +1,5 @@
+//.......................Empty Array by Substituting New Array.......................
+
 function emptyArray(arr) {
 
     // substituting new array
@@ -7,3 +9,27 @@ function emptyArray(arr) {
 }
 
 const array = [1, 2 ,3];
+const result = emptyArray(array);
+console.log(result);
+
+
+//.......................Empty Array Using splice()................................
+
+function emptyArray(arr) {
+
+    // substituting new array
+    arr.splice(0, arr.length);
+    return arr;
+}
+
+const array2 = [1, 2 ,3];
+// call the function
+const result2 = emptyArray(array);
+console.log(result2);
+
+// In the splice() method,
+
+// The first argument is the index of an array to start removing an item from.
+// The second argument is the number of elements that you want to remove from the index element.
+
+
