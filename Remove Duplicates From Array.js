6e50c2bbfc43ccf0e5bd542a6b6987908dc899a5
Apoorv <apoorvsharma494@gmail.com>
2022-04-13 23:57:56 +0530

    commit

--- Remove Duplicates From Array.js
+++ Remove Duplicates From Array.js
@@ -22,3 +22,25 @@ const array = [1, 2, 3, 2, 3];
 getUnique(array);
 
 // the indexOf() method returns -1 if the element is not in the array. Hence, during each iteration, if the element equals -1, the element is added to uniqueArr using push().
+
+
+
+//..........Using Set........................
+
+// program to remove duplicate value from an array
+
+function getUnique(arr){
+
+    // removing duplicate
+    let uniqueArr = [...new Set(arr)];
+
+    console.log(uniqueArr);
+}
+
+const array2 = [1, 2, 3, 2, 3];
+
+// calling the function
+getUnique(array2);
+
+// The array is converted to Set and all the duplicate elements are automatically removed.
+// The spread syntax ... is used to include all the elements of the Set to a new array.
