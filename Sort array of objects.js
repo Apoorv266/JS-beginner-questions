3befe37b90b22a4e03eebbbb8a6716f44bfe2500
Apoorv <apoorvsharma494@gmail.com>
2022-04-16 23:57:09 +0530

    commit

--- Sort array of objects.js
+++ Sort array of objects.js
@@ -12,3 +12,28 @@ const students = [{name: 'Sara', age:24},{name: 'John', age:22}, {name: 'Jack', 
 console.log(students.sort(compareAge)); 
 
 // the sort() method is used to sort an array element by the age property.
+
+
+//...........................Sort Array by Property Name......................
+
+// program to sort array by property name
+
+function compareName(a, b) {
+
+    // converting to uppercase to have case-insensitive comparison
+    const name1 = a.name.toUpperCase();
+    const name2 = b.name.toUpperCase();
+
+    let comparison = 0;
+
+    if (name1 > name2) {
+        comparison = 1;
+    } else if (name1 < name2) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+const students2 = [{name: 'Sara', age:24},{name: 'John', age:24}, {name: 'Jack', age:25}];
+
+console.log(students2.sort(compareName));
