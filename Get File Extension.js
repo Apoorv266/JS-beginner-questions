4c0cf6b5867b63981191ce3eae8fed7ba65aec68
Apoorv <apoorvsharma494@gmail.com>
2022-04-21 01:26:21 +0530

    commit

--- Get File Extension.js
+++ Get File Extension.js
@@ -3,6 +3,6 @@
 function getFileExtension(item){
     const extension = item.split('.').pop()
 }
-
+// The last array element, which is the extension, is returned using the pop() method.
 const result1 = getFileExtension('module.js');
 console.log(result1);
