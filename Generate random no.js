3cbedf8f87ced40f9e17960e2c08e3cc6f484967
Apoorv <apoorvsharma494@gmail.com>
2022-04-22 23:56:58 +0530

    commit

--- Generate random no.js
+++ Generate random no.js
@@ -1,3 +1,14 @@
 // If you want to find a random integer in between min (inclusive) to max (inclusive), you can use the following formula:
 
 // Math.floor(Math.random() * (max - min + 1)) + min
+
+
+
+//...........................Integer Value Between Two Numbers...........................
+
+const min = parseInt(prompt("Enter a min value: "));
+const max = parseInt(prompt("Enter a max value: "));
+
+const a = Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(`Random value between ${min} and ${max} is ${a}`);
