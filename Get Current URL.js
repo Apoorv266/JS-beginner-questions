535f8136dd1b691c9b389e5219a63526a78f0188
Apoorv <apoorvsharma494@gmail.com>
2022-04-23 23:55:42 +0530

    commit

--- Get Current URL.js
+++ Get Current URL.js
@@ -4,3 +4,5 @@ const url1 = window.location.href;
 const url2 = document.URL;
 console.log(url1);
 console.log(url2);
+
+// Both the window.location.href and the document.URL properties return the URL of the current page.
