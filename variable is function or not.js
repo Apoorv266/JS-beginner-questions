5add548ea6d43268eb94d5df3a7f41587e8d521d
Apoorv <apoorvsharma494@gmail.com>
2022-04-25 23:50:20 +0530

    commit

--- variable is function or not.js
+++ variable is function or not.js
@@ -1,4 +1,5 @@
 
+//................................using typeof.....................................
 
 function testVariable(variable) {
 if(typeof variable === "function"){
@@ -17,3 +18,21 @@ testVariable(x);
 
 // typeof operator is used with strict equal to === operator to check the type of variable.
 // The typeof operator gives the variable data type. === checks if the variable is equal in terms of value as well as the data type.
+
+//................................using instanceof.....................................
+
+function testVariable2(variable) {
+    if(typeof variable === "function"){
+    console.log("variable is a function")
+    }
+    else{
+        console.log("variable is not a function")
+    }
+    }
+    
+    const x2 = function() {
+        console.log('hello')
+    };
+    
+    testVariable2(x);
+
