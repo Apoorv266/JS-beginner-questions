6d29ab423e18a183b7fa44112d297340807a12f4
Apoorv <apoorvsharma494@gmail.com>
2022-04-26 23:56:15 +0530

    vommit

---
+++ Work With Constants.js
@@ -0,0 +1,26 @@
+// program to include constants
+
+const a = 5;
+console.log(a);                                 //    5
+
+// constants are block-scoped
+{
+    const a = 50;
+    console.log(a);                             //    50
+}
+console.log(a);                                 //    5
+
+const arr = ['work', 'exercise', 'eat'];
+console.log(arr);                               //    ["work", "exercise", "eat"]
+ 
+// add elements to arr array
+arr[3] = 'hello';
+console.log(arr);                              //   ["work", "exercise", "eat", "hello"]
+
+// the following code gives error
+// changing the value of a throws an error
+// uncomment to verify
+// a = 8;
+
+// throws an error
+// const x; 
