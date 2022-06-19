e966afa06c5a7f9f90b33a1034806dbed005b583
Apoorv <apoorvsharma494@gmail.com>
2022-06-19 16:16:22 +0530

    commit

--- Count characters.js
+++ Count characters.js
@@ -1,10 +1,10 @@
-let str = "abccz"
+let str = "hell f h"
 countChar(str)
 
 function countChar(str) {
     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    let newArr = Array.from(str)
+    let newArr = str.split("")
     console.log(newArr)
 
     let charObj = {}
@@ -13,8 +13,10 @@ function countChar(str) {
     for (let j = 0; j < newArr.length; j++) {
         for (let i = 0; i < alphabet.length; i++) {
             if (newArr[j] === alphabet[i]) {
-                charObj[newArr[j]] = 1;
                if(charObj.hasOwnProperty(newArr[j])){
+                    charObj[newArr[j]] = 1 + 1;
+                }
+                else{
                     charObj[newArr[j]] = 1;
                 }
             }
