1b23a91055ab36c7807b0c9ea1e879932242c036
Apoorv <apoorvsharma494@gmail.com>
2022-06-20 22:59:05 +0530

    commit

--- Check squared values in 2 arrays.js
+++ Check squared values in 2 arrays.js
@@ -1,21 +1,21 @@
 // create a program to check if value in one array are squared values in 2nd array
 let arr1 = [2, 3, 4]
 
-let arr2 = [4, 9, 16]
+let arr2 = [42, 92, 16]
 
 
 function checkSqa(arr1, arr2) {
 
     for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; i < arr2.length; j++) {
+        for (let j = 0; j < arr2.length; j++) {
 
             if (arr1[i] * arr1[i] === arr2[j]) {
                 console.log("All items are double")
             }
 
-            else{
-                console.log("All items are not double")
-            }
+            // else{
+            //     console.log("All items are not double")
+            // }
 
         }
 
