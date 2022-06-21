e384d2fdeaacd4109c186646403c24641813ec9a
Apoorv <apoorvsharma494@gmail.com>
2022-06-22 02:31:37 +0530

    commit

--- sum of array items is 0.js
+++ sum of array items is 0.js
@@ -68,15 +68,15 @@ let arr9 = [2, 4, 5 , 4, 5, 5, 5, 2, 4, 7, 8, 9, 5, 6, 8]
 console.log(checkUnique2(arr9))
 
 function checkUnique2(arr9) {
-    if(arr9.length === 0) return 0;
-    let i = 0
-    for (let j = 1; j < arr9.length; j++) {
-        if (arr9[i] !== arr9[j]) {
-            i++;
-            arr9[i] = arr9[j];
+    if(arr9.length === 0) return 0;  // if the length of the array is 0 then we return 0
+    let i = 0                      // initially we will set value of i = 0
+    for (let j = 1; j < arr9.length; j++) {  // we will start looping array from index 1 and one by one matches it with item at 0
+        if (arr9[i] !== arr9[j]) { // if item at 0 is not equal to item next to it then :-
+            i++;   // i will be increased by 1 
+            arr9[i] = arr9[j]; // and value of unmatched item in array 2 will take the place of array 1
         }
     }
-    return i + 1
+    return i + 1  // now till index 1 we have unqiue values and in order to get no of elements we till i will add 1
 }
 
 
