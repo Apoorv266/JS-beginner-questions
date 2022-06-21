3df3acb383480ff57596ad0460eee4e5599c6b65
Apoorv <apoorvsharma494@gmail.com>
2022-06-21 23:54:08 +0530

    commit

--- sum of array items is 0.js
+++ sum of array items is 0.js
@@ -1,9 +1,10 @@
+let arr1 = [-3, -2, -1, 0, 1, 2, 3]
 
-for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
+for (let i = 0; i < arr1.length; i++) {
+    for (let j = i + 1; j < arr1.length; j++) {
 
         if (arr1[i] + arr1[j] === 0) {
-            console.log(arr1[i], arr1[j])
+        console.log(arr1[i], arr1[j])
         }
     }
 
@@ -17,5 +18,3 @@ for (let i = 0; i < array.length; i++) {
 
 
 
-
-let arr1 = [-3, -2, -1, 0, 1, 2, 3]
