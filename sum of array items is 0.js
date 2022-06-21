8626877b5db458be4a9fb3b631e3144f28a088c2
Apoorv <apoorvsharma494@gmail.com>
2022-06-22 00:27:33 +0530

    commit

--- sum of array items is 0.js
+++ sum of array items is 0.js
@@ -1,25 +1,30 @@
-let arr1 = [-3, -2, -1, 0, 1, 2, 3]
+// two pointer problem
 
-for (let i = 0; i < arr1.length; i++) {
-    for (let j = i + 1; j < arr1.length; j++) {
 
-        if (arr1[i] + arr1[j] === 0) {
-        console.log(arr1[i], arr1[j])
-        }
-    }
+// let arr1 = [-3, -2, -1, 0, 1, 2, 3]
 
-}
+// for (let i = 0; i < arr1.length; i++) {
+//     for (let j = i + 1; j < arr1.length; j++) {
+
+//         if (arr1[i] + arr1[j] === 0) {
+//         console.log(arr1[i], arr1[j])
+//         }
+//     }
+
+// }
 
 //............using multiple pointer.......................
 
 
-let arr2 = [-3, -2, -1, 0, 1, 2, 3]
 
-sumZero(arr2)
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]))
 
 function sumZero(arr2){
     let left = 0;
     let right = arr2.length - 1
+    console.log(arr2.length)
+    console.log(right)
 
     while (left < right) {
         let sum = arr2[left] + arr2[right]
